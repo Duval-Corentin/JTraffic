684d1ab513774f7a18d0d8335bf423e40dab4117
Duval-Corentin <corentin.duval9@gmail.com>
2019-04-11 23:26:34 +0200

    add template

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("template01.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1600, 900));
         primaryStage.show();
     }
 

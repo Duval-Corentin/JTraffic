1174c95c20d44cabff4b86bd2f10214b91d926c6
Duval-Corentin <corentin.duval9@gmail.com>
2019-05-06 09:48:16 +0200

    progress in city

--- src/sample/Hub/City/City.java
+++ src/sample/Hub/City/City.java
@@ -1,10 +1,33 @@
 package sample.Hub.City;
 
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import sample.Hub.Hub;
+import javafx.scene.shape.*;
+
 
 public class City extends Hub {
 
-    public City() {
+    private static int width = 200;
+    private static int height = 100;
 
+    private Rectangle rect;
+
+    private Pane root;
+
+    public City(final Pane root, final int xPos, final int yPos, final String name, final String color) {
+
+        this.root = root;
+
+        rect = new Rectangle();
+        rect.setX(xPos);
+        rect.setY(yPos);
+        rect.setWidth(width);
+        rect.setHeight(height);
+        rect.setArcWidth(20);
+        rect.setArcHeight(20);
+        rect.setFill(Color.RED);
+
+        this.root.getChildren().add(this.rect);
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,29 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import sample.Hub.City.City;
+
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("template01.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1600, 900));
-        primaryStage.show();
-    }
+    private Pane root;
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("J Traffic");
+
+        this.root = new Pane();
+        this.root.setPrefSize(1600, 900);
+        primaryStage.setScene( new Scene(this.root));
+        primaryStage.show();
+
+        City Lille = new City(this.root, 600, 600, "Lille");
+    }
 
     public static void main(String[] args) {
         launch(args);
